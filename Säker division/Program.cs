57e488e689ae2e290ba302c6957c3bae93e7075d
eskoso <walteresko25@gmail.com>
2026-09-15 15:14:03 +0200

    secure division

---
+++ Säker division/Program.cs
@@ -0,0 +1,12 @@
+﻿double numerator;
+double denominator;
+double result;
+
+Console.WriteLine("mata in täljare:");
+numerator = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("mata in nämnare:");
+denominator = Convert.ToDouble(Console.ReadLine());
+
+result = denominator != 0 ? numerator / denominator : 0;
+Console.WriteLine(denominator != 0 ? $"Resultat: {result}" : $"Resultat: {result} - Fel: nämnaren får inte vara 0.");
