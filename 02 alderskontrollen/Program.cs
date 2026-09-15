f5821cb3001155964d131b8ea8949f68780e9d8b
eskoso <walteresko25@gmail.com>
2026-09-15 14:27:48 +0200

    Ålderskontrollen

---
+++ 02 alderskontrollen/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("What is your age?:");
+int age = int.Parse(Console.ReadLine());
+
+if (age < 18)
+{
+    Console.WriteLine("You are a minor.");
+}
+else if (age >= 18 && age < 65)
+{
+    Console.WriteLine("You are an adult.");
+}
+else
+{
+    Console.WriteLine("You are a senior citizen.");
+}
