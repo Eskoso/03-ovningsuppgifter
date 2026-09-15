0ca80a6b154d5dc7c590dc8b054959d9fdf706a6
eskoso <walteresko25@gmail.com>
2026-09-15 14:41:06 +0200

    Godisdelaren

---
+++ Godisdelaren/Program.cs
@@ -0,0 +1,8 @@
+﻿int persons = 4;
+int totalCandy = 23;
+
+int candyPerPerson = totalCandy / persons;
+int remainingCandy = totalCandy % persons;
+
+Console.WriteLine($"Varje person får {candyPerPerson} godisbitar.");
+Console.WriteLine($"Det blir {remainingCandy} godisbitar över.");
