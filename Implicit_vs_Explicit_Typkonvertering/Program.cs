0008c3c28dca6ee97e16b37225d47dc815c3541d
eskoso <walteresko25@gmail.com>
2026-09-15 14:44:31 +0200

    Implicit vs Explicit Typkonvertering

---
+++ Implicit_vs_Explicit_Typkonvertering/Program.cs
@@ -0,0 +1,10 @@
+﻿int tal = 500;
+
+double nyttIntTal = tal; // Implicit conversion from int to double
+
+double stortTal = 123.45;
+
+int nyttdoubleTal = (int)stortTal; // Explicit conversion from double to int
+
+Console.WriteLine($"Implicit conversion: {nyttIntTal}");
+Console.WriteLine($"Explicit conversion: {nyttdoubleTal}");
