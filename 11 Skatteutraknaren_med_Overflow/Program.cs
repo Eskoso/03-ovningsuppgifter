59a727bd22a8876c9821253563fe37e8e74e4ace
eskoso <walteresko25@gmail.com>
2026-09-15 15:19:19 +0200

    skatteutraknaren med overflowexception

---
+++ 11 Skatteutraknaren_med_Overflow/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1 = int.MaxValue;
+int number2 = 2;
+
+int result = unchecked(number1 * number2);
+Console.WriteLine($"Unchecked: {result}");
+
+try
+{
+    result = checked(number1 * number2);
+    Console.WriteLine($"Checked: {result}");
+}
+catch (OverflowException e)
+{
+    Console.WriteLine($"Checked: Talet blev för stort!");
+}
