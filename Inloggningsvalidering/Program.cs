bf1c23efe6129af3d491432074dbe06537b6302d
eskoso <walteresko25@gmail.com>
2026-09-15 14:38:31 +0200

    Inloggningsvalidering

---
+++ Inloggningsvalidering/Program.cs
@@ -0,0 +1,21 @@
+﻿string correctUsername = "admin";
+string correctPassword = "password";
+
+Console.Write("Användarnamn: ");
+string username = Console.ReadLine() ?? "";
+
+Console.Write("Lösenord: ");
+string password = Console.ReadLine() ?? "";
+
+
+if (username == correctUsername && password == correctPassword)
+{
+    Console.WriteLine("Inloggning lyckades!");
+} else if (username == "" || password == "")
+{
+    Console.WriteLine("Användarnamn och lösenord får inte vara tomma.");
+}
+else
+{
+    Console.WriteLine("Fel användarnamn eller lösenord.");
+}
